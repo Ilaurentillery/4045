1208dc8cd81e7082874b151085c3291c2f94ce2a
Administrator <Administrator@DESKTOP-099UQRG>
2019-09-23 17:15:50 -0400

    Inital commit of Spring Boot Project

---
+++ src/main/java/com/goldenretriever/GoldenRetrieversApplication.java
@@ -0,0 +1,13 @@
+package com.goldenretriever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldenRetrieversApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldenRetrieversApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goldenretriever/GoldenRetrievers/GoldenRetrieversApplicationTests.java
@@ -0,0 +1,16 @@
+package com.goldenretriever.GoldenRetrievers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GoldenRetrieversApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
